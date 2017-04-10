9b3d78a5cd76ccd18771e45d3deea922393b1618
elsayed abd elnaby <elsayed.fcis.1@gmail.com>
2017-04-10 20:59:32 +0200

    handle request with parameters

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 var express = require('express'),
     app = express();
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.send('<h1>Hello</h1> Express');
 });
 
-app.get('/home', function(req, res){
+app.get('/home', function (req, res) {
     res.send('<h1>Hello</h1> Express');
-})
+});
 
-app.get('*', function(req, res){
-    res.send('404 Page not found');
-})
+app.get('/who/:name?/:title?', function (req, res) {
+    var name = req.params.name,
+        title = req.params.title;
+        console.log(req.params);
+    res.send('<p>name: ' + name + '<br>title: ' + title + '</p>');
+});
 
-var server = app.listen(300, function(){
+app.get('*', function (req, res) {
+    res.send('<h1>404 Error, Page not found<h1>');
+});
+
+var server = app.listen(300, function () {
     console.log('Listening on port 300');
 });
