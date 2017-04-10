a6e7ee592d53a9460022f70748a0747f7da1f587
elsayed abd elnaby <elsayed.fcis.1@gmail.com>
2017-04-10 20:48:44 +0200

    handle not vaild requests

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get('/home', function(req, res){
     res.send('<h1>Hello</h1> Express');
 })
 
+app.get('*', function(req, res){
+    res.send('404 Page not found');
+})
+
 var server = app.listen(300, function(){
     console.log('Listening on port 300');
 });
