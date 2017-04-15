62a1e9d19e21237a3a5190d9e7d1085f4498f286
elsayed abd elnaby <elsayed.fcis.1@gmail.com>
2017-04-15 15:15:03 +0200

    create routes module

--- app.js
+++ app.js
@@ -1,21 +1,14 @@
 var express = require('express'),
-    app = express();
+    app = express(),
+    routes = require('./routes');
 
 app.set('view engine', 'ejs');
 
 app.locals.pagetitle = 'Awesome Website';
 
-app.get('/', function (req, res) {
-    res.render('default', { title: 'Home', classname: 'home', users: ['elsayed', 'alaraby', 'mohamed', 'hassan'] });
-});
-
-app.get('/home', function (req, res) {
-    res.render('default', { title: 'Home', classname: 'home', users: ['elsayed', 'alaraby', 'mohamed', 'hassan'] });
-});
-
-app.get('/about', function (req, res) {
-    res.render('default', { title: 'About Us', classname: 'about' });
-});
+app.get('/', routes.index);
+app.get('/home', routes.index);
+app.get('/about', routes.about);
 
 app.get('*', function (req, res) {
     res.send('<h1>404 Error, Page not found<h1>');
